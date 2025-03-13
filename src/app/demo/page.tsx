2d0308663870731432eb7d0c777253f07d16ae02
animesh <phb21013@tezu.ac.in>
2025-03-14 03:19:05 +0530

    devopssss

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,10 +1,14 @@
-import Image from 'next/image';
+import dynamic from 'next/dynamic';
 import React from 'react';
-// import Home from '../../../tmp/demo/page';
+
+// Dynamically import the generated page
+// @ts-ignore
+const Home = dynamic(() => import('../../../tmp/demo/page').catch(() => () => <div>Page not found</div>), { ssr: false });
+
 export default function HomePage() {
     return (
         <div>
-            {/* <Home/> */}
+            <Home />
         </div>
     );
 }
